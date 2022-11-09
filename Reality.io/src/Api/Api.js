478a2952aa87210952a7e072769c3eeb6005741a
Nirmal403 <nirmalcool414@gmail.com>
2022-11-09 18:54:01 +0530

    add to cart added

---
+++ Reality.io/src/Api/Api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://masai-hotel30.herokuapp.com"
+    // baseURL: "https://boiling-brushlands-36073.herokuapp.com"
+
+})
+
+//Api
